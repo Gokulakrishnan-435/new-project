f6c36b4ede06431c8bb93eba0d78a6c86bf8e45d
Gokul <krishguru435@gmail.com>
2022-02-23 15:27:46 +0530

    uicrud api express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './App';
+// import GalleryProvider from './ContextApi/Gallery';
+import './index.css'
+
+// ReactDOM.render(<GalleryProvider><App/></GalleryProvider>,document.getElementById('root'))
+
+ReactDOM.render(<App/>,document.getElementById('root'))
